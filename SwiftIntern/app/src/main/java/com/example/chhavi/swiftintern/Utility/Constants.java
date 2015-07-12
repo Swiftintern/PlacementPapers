e8048f647fde2f60ac431128e75d13c640a6d3ee
chhavip <coolaqua12@yahoo.co.in>
2015-07-12 20:43:28 +0530

    rate of play store added

--- SwiftIntern/app/src/main/java/com/example/chhavi/swiftintern/Utility/Constants.java
+++ SwiftIntern/app/src/main/java/com/example/chhavi/swiftintern/Utility/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
     public static String NO_NETWORK_FOUND  = "No internet connection was found. Please connect and try again";
     public static String VOLLEY_ERROR  = "Some error occured, please try again";
     public static String SAVE_PAPER  = "The experience paper has been saved for you, you can view it any time";
-    public static String APP_PACKAGE_NAME  = "com.";
+    public static String APP_PACKAGE_NAME  = "com.chhavi.swiftintern";
 
 }
